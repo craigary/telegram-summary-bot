09dd561de1f624cb6b737d5d1ff3fa6e1f71f31e
Craig Hart <i@craigary.net>
2025-03-20 00:20:01 +0800

    refactor: standardize string quotes and improve formatting in isJpeg, openai, and og modules

--- src/openai.ts
+++ src/openai.ts
@@ -1,8 +1,8 @@
-import OpenAI from "openai";
+import OpenAI from 'openai';
 export function getGenModel(env: Env) {
-	const apiKey = "my api key"; // defaults to process.env["OPENAI_API_KEY"]
-	const accountId = "{account_id}";
-	const gatewayId = "{gateway_id}";
+	const apiKey = 'my api key'; // defaults to process.env["OPENAI_API_KEY"]
+	const accountId = '{account_id}';
+	const gatewayId = '{gateway_id}';
 	const baseURL = `https://gateway.ai.cloudflare.com/v1/${accountId}/${gatewayId}/openai`;
 
 	const openai = new OpenAI({
@@ -11,15 +11,17 @@ export function getGenModel(env: Env) {
 	});
 	return {
 		generateContent: async function (prompt: string) {
-			const model = "gpt-3.5-turbo-0613";
-			const messages = [{ role: "user", content: prompt }];
+			const model = 'gpt-3.5-turbo-0613';
+			const messages = [{ role: 'user', content: prompt }];
 			const maxTokens = 100;
-			return (await openai.chat.completions.create({
-				model,
-				//@ts-ignore
-				messages,
-				max_tokens: maxTokens,
-			})).choices[0].message.content!;
-		}
-	}
+			return (
+				await openai.chat.completions.create({
+					model,
+					//@ts-ignore
+					messages,
+					max_tokens: maxTokens,
+				})
+			).choices[0].message.content!;
+		},
+	};
 }
